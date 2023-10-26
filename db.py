f1e193dadc884652426151aa60a6ef7c038fe429
Bader alotaibi <baderalotaibi3@gmail.com>
2023-10-26 11:49:25 +0300

    add more funcs like add users

--- db.py
+++ db.py
@@ -1,59 +1,82 @@
 import mysql.connector
-
 # Connect to the MySQL database
-conn = mysql.connector.connect(
-    host="localhost",
-    user="your_username",
-    password="your_password"
-)
+def connect_to_database():
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="your_username",
+            password="your_password",
+            database="forum_db"  # Assuming your database is already created
+        )
+        return conn
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
 
-# Create a cursor object to execute SQL commands
-cur = conn.cursor()
+def close_connection(conn):
+    conn.close()
 
-# Create the database (if it doesn't exist)
-cur.execute("CREATE DATABASE IF NOT EXISTS forum_db")
+def create_tables(conn):
+    try:
+        cursor = conn.cursor()
+        
+        # Create the users table
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                user_id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(50) UNIQUE NOT NULL,
+                password VARCHAR(50) NOT NULL,
+                email VARCHAR(50) UNIQUE NOT NULL
+            );
+        """)
 
-# Use the forum_db database
-cur.execute("USE forum_db")
+        # Create the threads table
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS threads (
+                thread_id INT AUTO_INCREMENT PRIMARY KEY,
+                title VARCHAR(100) NOT NULL,
+                content TEXT NOT NULL,
+                user_id INT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(user_id)
+            );
+        """)
 
-# Create the users table
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        user_id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(50) UNIQUE NOT NULL,
-        password VARCHAR(50) NOT NULL,
-        email VARCHAR(50) UNIQUE NOT NULL
-    );
-""")
+        # Create the comments table
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS comments (
+                comment_id INT AUTO_INCREMENT PRIMARY KEY,
+                content TEXT NOT NULL,
+                user_id INT,
+                thread_id INT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(user_id),
+                FOREIGN KEY (thread_id) REFERENCES threads(thread_id)
+            );
+        """)
 
-# Create the threads table
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS threads (
-        thread_id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(100) NOT NULL,
-        content TEXT NOT NULL,
-        user_id INT,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (user_id) REFERENCES users(user_id)
-    );
-""")
+        conn.commit()
+        cursor.close()
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
 
-# Create the comments table
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS comments (
-        comment_id INT AUTO_INCREMENT PRIMARY KEY,
-        content TEXT NOT NULL,
-        user_id INT,
-        thread_id INT,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (user_id) REFERENCES users(user_id),
-        FOREIGN KEY (thread_id) REFERENCES threads(thread_id)
-    );
-""")
+def addUser(username, password, email):
+    conn=connect_to_database()
+    try:
+        cursor = conn.cursor()
+        query = "INSERT INTO users (username, password, email) VALUES (%s, %s, %s)"
+        values = (username, password, email)
+        cursor.execute(query, values)
+        conn.commit()
+        cursor.close()
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
 
-# Commit the changes to the database
-conn.commit()
+# Similar functions for addThread, addComment, getUser, getThread, getComment, getThreads, getComments
 
-# Close the cursor and connection
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    conn = connect_to_database()
+    if conn:
+        create_tables(conn)
+        # You can call your database functions here
+        close_connection(conn)
--- user.py
+++ user.py
@@ -1,29 +1,63 @@
+from db import addUser 
 class User:
-    def __init__(self, username, email):
+    def __init__(self, username, password,email):
+        """
+        Initialize a User object.
+
+        :param username: The username of the user.
+        :param email: The email of the user.
+        """
         self.username = username
         self.email = email
         self.threads = []
+        addUser(username, password, email)
+        
 
     def create_thread(self, title, content):
-        thread = thread(title, content, self)
+        """
+        Create a new thread associated with this user.
+
+        :param title: The title of the thread.
+        :param content: The content of the thread.
+        :return: The created Thread object.
+        """
+        thread = Thread(title, content, self)
         self.threads.append(thread)
         return thread
 
-class thread:
+class Thread:
     def __init__(self, title, content, author):
+        """
+        Initialize a Thread object.
+
+        :param title: The title of the thread.
+        :param content: The content of the thread.
+        :param author: The user who created the thread.
+        """
         self.title = title
         self.content = content
         self.author = author
         self.comments = []
 
     def add_comment(self, content, author):
+        """
+        Add a new comment to the thread.
+
+        :param content: The content of the comment.
+        :param author: The user who created the comment.
+        :return: The created Comment object.
+        """
         comment = Comment(content, author)
         self.comments.append(comment)
         return comment
 
 class Comment:
     def __init__(self, content, author):
+        """
+        Initialize a Comment object.
+
+        :param content: The content of the comment.
+        :param author: The user who created the comment.
+        """
         self.content = content
         self.author = author
-
-
