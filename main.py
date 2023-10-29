8ea5f011503b688de3e8302d4940f7368ac2a292
Bader alotaibi <baderalotaibi3@gmail.com>
2023-10-29 11:17:37 +0300

    updated

--- main.py
+++ main.py
@@ -1,36 +1,26 @@
-from db import getThreadsInCategory , get_all_categories
-from user import User , Thread , Comment
+from db import getThreadsInCategory, get_all_categories
+from user import User, Thread, Comment
 from art import tprint
 from colorama import Back
-import hashlib 
+import hashlib
 import re
 
 def is_valid_email(email):
     # Regular expression pattern for a basic email validation
     pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    
-    # Use the re.match function to check if the email matches the pattern
-    if re.match(pattern, email):
-        return True
-    else:
-        return False
+    return bool(re.match(pattern, email))
 
 def allCategories():
-    for i in get_all_categories():
-            print(i)
+    for category in get_all_categories():
+        print(category)
 
-def printAllThreads(i):
-        threads = getThreadsInCategory(i)  # Replace with the category name you want to retrieve
-        if threads:
-            for thread in threads:
-                print(f"Thread ID: {thread[0]}, Title: {thread[1]}, Content: {thread[2]}, Created At: {thread[3]}, Author: {thread[4]}")
-        else:
-            print("No threads found in the specified category.")
-
-
-def cliView():
-    allCategories()
-    printAllThreads(input("Enter The name of what category you want to see its threads:"))
+def printAllThreads(category_name):
+    threads = getThreadsInCategory(category_name)
+    if threads:
+        for thread in threads:
+            print(f"Thread ID: {thread[0]}, Title: {thread[1]}, Content: {thread[2]}, Created At: {thread[3]}, Author: {thread[4]}")
+    else:
+        print("No threads found in the specified category.")
 
 def registerUser():
     username = input("Enter your username: ")
@@ -38,11 +28,10 @@ def registerUser():
     password = hashlib.sha256(password.encode()).hexdigest()
     email = input("Enter your email: ")
     if not is_valid_email(email):
-        print(Back.RED, "Invalid email address")
+        print(Back.RED + "Invalid email address")
         return registerUser()
     user = User(username, password, email)
-    print(Back.GREEN,f"Created a new user with user ID {user.userId}")
-    
+    print(Back.GREEN + f"Created a new user with user ID {user.userId}")
 
 def login():
     username = input("Enter your username: ")
@@ -50,26 +39,23 @@ def login():
     password = hashlib.sha256(password.encode()).hexdigest()
     user = User.login(username, password)
     if user:
-        print(Back.GREEN,f"Logged in as user ID {user.userId}")
+        print(Back.GREEN + f"Logged in as user ID {user.userId}")
         return user
     else:
-        print(Back.RED, "Invalid username or password")
+        print(Back.RED + "Invalid username or password")
         return login()
 
-def addThread():
-    login = login()
+def addThread(user):
     content = input("Enter the content of the thread: ")
     title = input("Enter the title of the thread: ")
-    thread = Thread(title, content, login.user)
-    print(Back.GREEN,f"Created a new thread with thread ID {thread.thread_id}")
+    thread = Thread(title, content, user)
+    print(Back.GREEN + f"Created a new thread with thread ID {thread.thread_id}")
 
-def addComment():
-    login = login()
+def addComment(user):
     content = input("Enter the content of the comment: ")
     thread_id = input("Enter the ID of the thread: ")
-    comment = Comment(thread_id, content, login.user)
-    print(Back.GREEN,f"Created a new comment with comment ID {comment.comment_id}")
-
+    comment = Comment(thread_id, content, user)
+    print(Back.GREEN + f"Created a new comment with comment ID {comment.comment_id}")
 
 def main():
     tprint("TECH", font='block')
@@ -97,27 +83,20 @@ def main():
         elif choice == "4":
             user = login()
         elif choice == "5":
-            try:
-                if user:
-                    addThread(user)
-            except:
-                print(Back.RED, "Please log in first.")
+            if user:
+                addThread(user)
             else:
-                print(Back.RED, "Please log in first.")
+                print(Back.RED + "Please log in first.")
         elif choice == "6":
             if user:
                 addComment(user)
             else:
-                print(Back.RED, "Please log in first.")
+                print(Back.RED + "Please log in first.")
         elif choice == "0":
             print("Goodbye!")
             break
         else:
-            print("Invalid choice. Please try again.")
-
-
+            print(Back.RED + "Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
-    
-    
