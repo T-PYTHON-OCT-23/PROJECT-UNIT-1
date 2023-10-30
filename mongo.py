10fc31b49a4cd8b2ccb4d52358e07306e9e41546
Bader alotaibi <baderalotaibi3@gmail.com>
2023-10-30 14:11:37 +0300

    fix bug

--- mongo.py
+++ mongo.py
@@ -8,34 +8,34 @@ class Mongo:
         self.db = self.cluster["cluster0"]    #database name
         print("MongoDB connected")
 
-    def addToMongo(self, thread_id, title, content, category, author):
-    # Create a document with the data
-        thread_data = {
-            "thread_id": thread_id,
-            "title": title,
-            "content": content,
-            "category": category,
-            "author": author
-        }
+def addToMongo(self, thread_id, title, content, category, author):
+# Create a document with the data
+    thread_data = {
+        "thread_id": thread_id,
+        "title": title,
+        "content": content,
+        "category": category,
+        "author": author
+    }
 
-        # Insert the document into a MongoDB collection
-        collection = self.db[category]
-        collection.insert_one(thread_data)
+    # Insert the document into a MongoDB collection
+    collection = self.db[category]
+    collection.insert_one(thread_data)
 
-    def getCategories(self, category):
-        for i in category:
-            self.db.create_collection(i)
-        
-    def getThreads(self, category):
-        collection = self.db[category]
-        return collection.find({})
+def getCategories(self, category):
+    for i in category:
+        self.db.create_collection(i)
+    
+def getThreads(self, category):
+    collection = self.db[category]
+    return collection.find({})
 
-    def getThredsById(self, category, id):
-        collection = self.db[category]
-        return collection.find({"thread_id": id})
+def getThredsById(self, category, id):
+    collection = self.db[category]
+    return collection.find({"thread_id": id})
 
-    def removeThread(self, category, id):
-        collection = self.db[category]
-        collection.delete_one({"thread_id": id})
-        
+def removeThread(self, category, id):
+    collection = self.db[category]
+    collection.delete_one({"thread_id": id})
+    
 techDB=Mongo()
