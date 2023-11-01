32be7768d4b1e8e8a86d457657b7e35294046402
naif <iimnaif@gmail.com>
2023-11-01 23:01:43 +0300

    fun.py

---
+++ fun.py
@@ -0,0 +1,11 @@
+from managerAccount import *
+from datetime import datetime
+from prettytable import PrettyTable
+from art import *
+from car import *
+from spareParts import spare_parts
+art_2=art("woman",number=2)
+from colorama import Fore, Back, Style,init
+import sys
+print(Style.RESET_ALL)
+init(autoreset=True)
